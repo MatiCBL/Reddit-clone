440722fed1832f883ca509d68e927b1813a4a4c5
MatiCBL <brzoza.mathew@gmail.com>
2023-05-19 22:35:18 +0100

    'custom-hook-added-for-community-snippets'

---
+++ src/hooks/useCommunityData.tsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRecoilState } from "recoil";
+import { Community, communityState } from "../atoms/communitiesAtom";
+import { auth, firestore } from "../firebase/clientApp";
+import { getDocs, collection } from "firebase/firestore";
+
+const useCommunityData = () => {
+  const [user] = useAuthState(auth);
+  const [communityStateValue, setCommunityStateValue] =
+    useRecoilState(communityState);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const onJoinOrLeaveCommunity = (
+    communityData: Community,
+    isJoined: boolean
+  ) => {
+    // is the user signer in?
+    // if not => open auth modal
+
+    if (isJoined) {
+      leaveCommunity(communityData.id);
+      return;
+    }
+    joinCommunity(communityData);
+  };
+
+  const getMySnippets = async () => {
+    setLoading(true);
+    try {
+      // get users snippets
+      const snippetDocs = await getDocs(
+        collection(firestore, `users/&{user?.uid}/communitySnippets}`)
+      );
+      const snippets = snippetDocs.docs.map((doc) => ({ ...doc.data() }));
+    } catch (error) {}
+  };
+
+  const joinCommunity = (communityData: Community) => {};
+
+  const leaveCommunity = (communityId: string) => {};
+
+  useEffect(() => {
+    if (!user) return;
+    getMySnippets();
+  }, [user]);
+  return {
+    // data and functions
+    communityStateValue,
+    onJoinOrLeaveCommunity,
+  };
+};
+export default useCommunityData;
