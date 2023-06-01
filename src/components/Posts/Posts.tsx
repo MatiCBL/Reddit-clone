e10fd5aa549f9af099dc58ea70764c41787955b4
MatiCBL <brzoza.mathew@gmail.com>
2023-06-01 23:46:08 +0100

    'added-funcitonality-to-nav-dropdown-for-communities'

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -52,7 +52,7 @@ const Posts: React.FC<PostsProps> = ({ communityData }) => {
   useEffect(() => {
     getPosts();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [communityData]);
 
   return (
     <>
